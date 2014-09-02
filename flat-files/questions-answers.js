ddf9a1920448402b184824b305e2e6d7b7717656
Sisi Wei <me@sisiwei.com>
2014-09-02 09:23:27 -0400

    light restructuring

--- flat-files/questions-answers.js
+++ flat-files/questions-answers.js
@@ -1,29 +1,42 @@
-var questionsAnswers = [
-	["Question 1", "Answer"],
-	["Question 2", "Answer"],
-	["Question 3", "Answer"],
-	["Question 4", "Answer"],
-	["Question 5", "Answer"],
-	["Question 6", "Answer"],
-	["Question 7", "Answer"],
-	["Question 8", "Answer"],
-	["Question 9", "Answer"],
-	["Question 10", "Answer"]
-];
-
-$(document).ready(function(){
-	populateQuestions();
-
-})
-
-function populateQuestions(){
-	
-	var html = ["<div class='row'><div class='col-md-6'>Questions</div><div class='col-md-3'>Low Estimate</div><div class='col-md-3'>High estimate</div>"];
-	
-	$.each(questionsAnswers, function(k,v){
-		html.push("<div class='row' data-question='question-" + k + "'><div class='col-md-6'>" + v[0] + "</div><div class='col-md-3'><input type='text' name='low'></div><div class='col-md-3'><input type='text' name='high'></div></div>");
-	});
-
-	$('.questions').html(html.join(''));
-
-}
+var questionsAnswers = {
+	"question-1": {
+			"question" : "What is 19 + 1?",
+			"answer" : 20
+	},	
+	"question-2": {
+			"question" : "What is 19 + 1?",
+			"answer" : 20
+	},	
+	"question-3": {
+			"question" : "What is 19 + 1?",
+			"answer" : 20
+	},	
+	"question-4": {
+			"question" : "What is 19 + 1?",
+			"answer" : 20
+	},	
+	"question-5": {
+			"question" : "What is 19 + 1?",
+			"answer" : 20
+	},	
+	"question-6": {
+			"question" : "What is 19 + 1?",
+			"answer" : 20
+	},	
+	"question-7": {
+			"question" : "What is 19 + 1?",
+			"answer" : 20
+	},	
+	"question-8": {
+			"question" : "What is 19 + 1?",
+			"answer" : 20
+	},	
+	"question-9": {
+			"question" : "What is 19 + 1?",
+			"answer" : 20
+	},	
+	"question-10": {
+			"question" : "What is 19 + 1?",
+			"answer" : 20
+	}
+};
---
+++ flat-files/script.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+	populateQuestions();
+
+	$('#guess').click(function(){
+
+		$.each($('.question-row'), function(k,v){
+			var q = $(this).attr('data-question');
+
+		});
+
+	});
+
+})
+
+function populateQuestions(){
+	
+	var html = ["<div class='row'><div class='col-md-6'>Questions</div><div class='col-md-3'>Low Estimate</div><div class='col-md-3'>High estimate</div>"];
+	
+	for (var i = 1; i <= 10; i++) {
+    html.push("<div class='row question-row' data-question='question-" + i + "'><div class='col-md-6'>" + questionsAnswers['question-' + i]['question'] + "</div><div class='col-md-3'><input type='text' name='low'></div><div class='col-md-3'><input type='text' name='high'></div></div>");
+	}
+
+	$('.questions').html(html.join(''));
+
+}
