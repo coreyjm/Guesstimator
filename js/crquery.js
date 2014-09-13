56d3845303bd6eaadf8211ba1638798400c8ce38
Rebekah Monson <rebekahmonson@gmail.com>
2014-09-13 14:47:12 -0400

    test w/ flickr

--- js/crquery.js
+++ js/crquery.js
@@ -1,2 +1,17 @@
 
-var city = "16000US1714000"
+var city = "16000US1714000";
+
+var flickerAPI = "http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?";
+  $.getJSON( flickerAPI, {
+    tags: "mount rainier",
+    tagmode: "any",
+    format: "json"
+  })
+    .done(function( data ) {
+      $.each( data.items, function( i, item ) {
+        $( "<img>" ).attr( "src", item.media.m ).appendTo( "#query" );
+        if ( i === 3 ) {
+          return false;
+        }
+      });
+    });
