7fd2da58e9641111fe4dbac2131c7fd544d4ecd2
Rebekah Monson <rebekahmonson@gmail.com>
2014-09-13 17:11:52 -0400

    w

--- js/crquery.js
+++ js/crquery.js
@@ -21,7 +21,7 @@ console.log(crAPI);
 $.getJSON(crAPI, function (json) {
 
 	console.log(json); //Check out all the data in the console.
-	console.log(json.release);
+	console.log(json.tables);
     //console.log(json.data.16000US1714000.B01001.estimate.B01001001);
     // Get the numbers we want from the various tables and store them as vars.
 
