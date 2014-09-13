1fc33ed7ae066069703874a666e8d30190940f4e
Rebekah Monson <rebekahmonson@gmail.com>
2014-09-13 16:35:55 -0400

    test

--- js/crquery.js
+++ js/crquery.js
@@ -18,10 +18,10 @@ var tables =
 var crAPI = "http://api.censusreporter.org/1.0/data/show/latest?table_ids="+tables+"&geo_ids="+city;
 //console.log(crAPI);
 
-$.getJSON(crAPI, function (data) {
+$.getJSON(crAPI, function (json) {
 
-	console.log(data); //Check out all the data in the console.
-    console.log(data.response.data.16000US1714000.B01001.estimate.B01001001);
+	console.log(json); //Check out all the data in the console.
+    console.log(json.data.16000US1714000.B01001.estimate.B01001001);
     // Get the numbers we want from the various tables and store them as vars.
 
 });
