e90f117072f29685704c67357b1cc5449696f21f
Rebekah Monson <rebekahmonson@gmail.com>
2014-09-13 12:44:15 -0400

    dump old stuff to start rebuilding a client-side version

---
+++ oldphpcrap/flat-files/questions-answers.js
@@ -0,0 +1,42 @@
+var questionsAnswers = {
+	"question-1": {
+			"question" : "What is 19 + 1?",
+			"answer" : 20
+	},	
+	"question-2": {
+			"question" : "What is 19 + 1?",
+			"answer" : 20
+	},	
+	"question-3": {
+			"question" : "What is 19 + 1?",
+			"answer" : 20
+	},	
+	"question-4": {
+			"question" : "What is 19 + 1?",
+			"answer" : 20
+	},	
+	"question-5": {
+			"question" : "What is 19 + 1?",
+			"answer" : 20
+	},	
+	"question-6": {
+			"question" : "What is 19 + 1?",
+			"answer" : 20
+	},	
+	"question-7": {
+			"question" : "What is 19 + 1?",
+			"answer" : 20
+	},	
+	"question-8": {
+			"question" : "What is 19 + 1?",
+			"answer" : 20
+	},	
+	"question-9": {
+			"question" : "What is 19 + 1?",
+			"answer" : 20
+	},	
+	"question-10": {
+			"question" : "What is 19 + 1?",
+			"answer" : 20
+	}
+};
