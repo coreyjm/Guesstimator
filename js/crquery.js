56e4ad2b317133b1e56d7a47ee0427b2078333fe
Rebekah Monson <rebekahmonson@gmail.com>
2014-09-13 17:29:11 -0400

    d

--- js/crquery.js
+++ js/crquery.js
@@ -21,9 +21,14 @@ console.log(crAPI);
 $.getJSON(crAPI, function (json) {
 
 	console.log(json); //Check out all the data in the console.
-	console.log(json.data);
-	console.log(json.data.16000US1714000);
+	console.log(json.data); //down to data level works
+	
+	//This chokes
+	//console.log(json.data.16000US1714000); 
+
+    //This is what I think the right thing should be:
     //console.log(json.data.16000US1714000.B01001.estimate.B01001001);
+    
     // Get the numbers we want from the various tables and store them as vars.
 
 });
