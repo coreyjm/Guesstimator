511c94c89a4178d0825bd8b3617b1749d6670bdb
Sisi Wei <me@sisiwei.com>
2014-09-24 20:36:09 -0400

    awesome prize language

--- js/script3.js
+++ js/script3.js
@@ -1,8 +1,6 @@
-
 var city= "Chicago"; //Change this for your city!
 var cityCode = "16000US1714000"; //Change this for your city!
 var score = 0;
-
 
 /*
 Now we make a big 'ol queries object full of all the individual queries with associated tables and 
@@ -188,7 +186,7 @@ $.getJSON(crAPI, function (crdata) {
 		if (!incomplete){
 			$('#guess').fadeOut();
 			var click = "window.open('https://twitter.com/intent/tweet?text=My%20%23score%20is%20' + (score*10) + '%25%20on%20the%20%23ona14newsgames%20Guesstimator!%20Try%20to%20beat%20me!&url=http%3A%2F%2Fbit.ly%2FONAguess', 'newwindow', 'width=430, height=260'); return false;";
-			$('#score').html('<h3>I just scored '+ (score*10) +'% on the #ona14newsgames Guesstimator! Try to beat me at http://bit.ly/ONAguess</h3><a onclick="' + click + '" href="" id="tweet" class="btn btn-info">Tweet Your Score!</a>');
+			$('#score').html('<h3>You scored '+ (score*10) +'%! Now tweet your score! If you do so during the session, you could get a prize! </h3><a onclick="' + click + '" href="" id="tweet" class="btn btn-info">Tweet Your Score!</a>');
 		}
 
 	});
