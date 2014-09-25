85743053f7e55a7ce88295696093b6b946a64ab3
Rebekah Monson <rebekahmonson@gmail.com>
2014-09-25 09:47:59 -0400

    Cleaning up

--- oldphpcrap/app/views/footer.htm
+++
@@ -1,8 +0,0 @@
-
-    </div>
-
-    <script src="http://code.jquery.com/jquery.js"></script>
-    <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
-
-    </body>
-</html>
--- oldphpcrap/tmp/004kvzeofgf.005u3dy1pwb.php
+++
@@ -1,4 +0,0 @@
-<?php echo $this->render('header.htm',$this->mime,get_defined_vars()); ?>
-<?php echo $this->render('nav.htm',$this->mime,get_defined_vars()); ?>
-<?php echo $this->render($view,$this->mime,get_defined_vars()); ?>
-<?php echo $this->render('footer.htm',$this->mime,get_defined_vars()); ?>
